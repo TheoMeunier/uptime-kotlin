09d4c84631f3db2f90e4acdd3af9725353a875f0
tmeunier <theo.meunier41@gmail.com>
2025-12-26 20:13:04 +0100

    feat: add pulse warning

--- front/src/features/probes/components/modules/probe-status.tsx
+++ front/src/features/probes/components/modules/probe-status.tsx
@@ -1,17 +1,36 @@
 import ProbeStatusEnum from "@/features/probes/enums/probe-status.enum.ts";
 
-export default function ProbeStatus({status} : {status: string}) {
-    return <div>
-        {status === ProbeStatusEnum.SUCCESS ? (
-            <span className="relative flex size-8">
-    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"/>
-    <span className="relative inline-flex size-8 rounded-full bg-green-500"/>
-  </span>
-        ) : (
-            <span className="relative flex size-8">
-    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"/>
-    <span className="relative inline-flex size-8 rounded-full bg-red-500"/>
-  </span>
-        )}
+export default function ProbeStatus({ status }: { status: string }) {
+  let pingColor = "";
+  let dotColor = "";
+
+  switch (status) {
+    case ProbeStatusEnum.SUCCESS:
+      pingColor = "bg-green-400";
+      dotColor = "bg-green-500";
+      break;
+
+    case ProbeStatusEnum.WARNING:
+      pingColor = "bg-orange-400";
+      dotColor = "bg-orange-500";
+      break;
+
+    default:
+      pingColor = "bg-red-400";
+      dotColor = "bg-red-500";
+      break;
+  }
+
+  return (
+    <div>
+      <span className="relative flex size-8">
+        <span
+          className={`absolute inline-flex h-full w-full animate-ping rounded-full ${pingColor} opacity-75`}
+        />
+        <span
+          className={`relative inline-flex size-8 rounded-full ${dotColor}`}
+        />
+      </span>
     </div>
+  );
 }
