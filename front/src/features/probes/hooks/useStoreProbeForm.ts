96fb324643c3160a70e02a8fbeb08c88eeba650d
tmeunier <theo.meunier41@gmail.com>
2026-01-15 18:50:30 +0100

    refactor(monitor): create form

--- front/src/features/probes/hooks/useStoreProbeForm.ts
+++ front/src/features/probes/hooks/useStoreProbeForm.ts
@@ -1,85 +1,15 @@
-import { z } from 'zod';
 import { data, useNavigate } from 'react-router';
-import { type SubmitHandler, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { type SubmitHandler } from 'react-hook-form';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import probeService from '@/features/probes/services/probeService.ts';
-import ProbeProtocol from '@/features/probes/enums/probe-enum.ts';
-import HttpStatusCode from '@/features/probes/enums/http-status-code.ts';
 import { toast } from 'sonner';
 import { useTranslation } from 'react-i18next';
+import type { StoreProbeSchema } from '@/features/probes/hooks/useProbeForm.ts';
 
-const baseStoreProbeSchema = z.object({
-	name: z.string().min(3).max(255),
-	interval: z.number().min(10).max(3600),
-	retry: z.number().min(1).max(10).optional(),
-	interval_retry: z.number().min(0).max(3600).optional(),
-	timeout: z.number().min(10).max(3600).optional(),
-	enabled: z.boolean().optional(),
-	description: z
-		.string()
-		.transform((val) => (val === '' ? null : val))
-		.nullish(),
-	notifications: z.array(z.uuid()).optional(),
-});
-
-const httpProbeSchema = baseStoreProbeSchema.extend({
-	protocol: z.literal('HTTP'),
-	notification_certificate: z.boolean(),
-	ignore_certificate_errors: z.boolean(),
-	http_code_allowed: z.enum(HttpStatusCode).array(),
-	url: z.url(),
-});
-
-const tcpProbeSchema = baseStoreProbeSchema.extend({
-	protocol: z.literal('TCP'),
-	url: z.ipv4(),
-	tcp_port: z.number().min(1).max(65535),
-});
-
-const pingProbeSchema = baseStoreProbeSchema.extend({
-	protocol: z.literal('PING'),
-	url: z.url(),
-	ping_heartbeat_interval: z.number().min(1).max(60),
-	ping_max_packet: z.number().min(1).max(10),
-	ping_size: z.number().min(32).max(65500),
-	ping_delay: z.number().min(1).max(60),
-	ping_numeric_output: z.boolean().optional(),
-});
-
-const dnsProbeSchema = baseStoreProbeSchema.extend({
-	protocol: z.literal('DNS'),
-	url: z.url(),
-	dns_server: z.ipv4(),
-	dns_port: z.number().min(1).max(65535).optional(),
-});
-
-export const storeProbeSchema = z.discriminatedUnion('protocol', [
-	httpProbeSchema,
-	tcpProbeSchema,
-	pingProbeSchema,
-	dnsProbeSchema,
-]);
-
-export type StoreProbeSchema = z.infer<typeof storeProbeSchema>;
-
-export function useStoreProbeForm() {
+export function useStoreMonitor() {
 	const { t } = useTranslation();
 	const navigate = useNavigate();
 	const queryClient = useQueryClient();
-
-	const form = useForm<StoreProbeSchema>({
-		resolver: zodResolver(storeProbeSchema),
-		defaultValues: {
-			protocol: ProbeProtocol.HTTP,
-			interval: 60,
-			interval_retry: 60,
-			retry: 3,
-			timeout: 30,
-			enabled: true,
-			notifications: [],
-		},
-	});
 
 	const mutation = useMutation({
 		mutationFn: async (data: StoreProbeSchema) => {
@@ -98,9 +28,7 @@ export function useStoreProbeForm() {
 	};
 
 	return {
-		form,
 		onsubmit,
 		isLoading: mutation.isPending,
-		errors: form.formState.errors,
 	};
 }
--- front/src/pages/probes/create-probe.tsx
+++ front/src/pages/probes/create-probe.tsx
@@ -1,5 +1,27 @@
-import CreateProbeForm from '@/features/probes/components/create-probe-form.tsx';
+import ProbeForm from '@/features/probes/components/forms/probe-form.tsx';
+import ProbeProtocol from '@/features/probes/enums/probe-enum.ts';
+import { useStoreMonitor } from '@/features/probes/hooks/useStoreProbeForm.ts';
 
 export default function CreateProbe() {
-	return <CreateProbeForm />;
+	const { isLoading, onsubmit } = useStoreMonitor();
+
+	const defaultValues = {
+		protocol: ProbeProtocol.HTTP,
+		interval: 60,
+		interval_retry: 60,
+		retry: 3,
+		timeout: 30,
+		enabled: true,
+		notifications: [],
+	};
+
+	return (
+		<>
+			<div className="mb-8">
+				<h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">Create monitor</h1>
+			</div>
+
+			<ProbeForm mode="create" onSubmit={onsubmit} isLoading={isLoading} defaultValues={defaultValues} />
+		</>
+	);
 }
