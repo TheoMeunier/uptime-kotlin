af8ef4c0a3b2b86f15b9febd48f357072a779905
tmeunier <theo.meunier41@gmail.com>
2026-02-07 09:57:07 +0100

    fix(auth): add configProperty for issuer

--- src/main/kotlin/tmenier/fr/auth/services/JwtService.kt
+++ src/main/kotlin/tmenier/fr/auth/services/JwtService.kt
@@ -2,12 +2,17 @@ package tmenier.fr.auth.services
 
 import io.smallrye.jwt.build.Jwt
 import jakarta.enterprise.context.ApplicationScoped
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import java.time.Duration
 import java.time.Instant
 import java.util.UUID
 
 @ApplicationScoped
 class JwtService {
+
+    @ConfigProperty(name = "mp.jwt.verify.issuer", defaultValue = "https://uptime-kotlin.theomeunier.fr")
+    private lateinit var jwtIssuer: String
+
     fun generateJwt(
         userId: UUID,
         username: String,
@@ -16,7 +21,7 @@ class JwtService {
         Jwt
             .claims()
             .subject(userId.toString())
-            .issuer("https://uptime-kotlin.theomeunier.fr/")
+            .issuer(jwtIssuer)
             .issuedAt(Instant.now())
             .expiresAt(Instant.now().plus(Duration.ofHours(10)))
             .claim("name", username)
