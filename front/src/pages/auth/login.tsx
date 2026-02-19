d9fdc93f785531431b8587b71a0ed4240d36513d
tmeunier <theo.meunier41@gmail.com>
2026-02-19 09:36:41 +0100

    feat: redesign login page

--- front/src/pages/auth/login.tsx
+++ front/src/pages/auth/login.tsx
@@ -2,8 +2,14 @@ import { LoginForm } from '@/features/auth/components/auth_form/login-form.tsx';
 
 export default function Login() {
 	return (
-		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-			<div className="w-full max-w-sm">
+		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+			<div className="flex w-full max-w-sm flex-col gap-6">
+				<a href="#" className="flex items-center gap-2 self-center font-medium">
+					<div className="text-primary-foreground flex size-6 items-center justify-center rounded-md">
+						<img src="/img/logo.png" alt="logo" />
+					</div>
+					Uptime kotlin
+				</a>
 				<LoginForm />
 			</div>
 		</div>
