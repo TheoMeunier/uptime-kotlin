6bc3eede5435d3eb3ba1c09676ead30b4f93b037
tmeunier <theo.meunier41@gmail.com>
2026-08-04 16:04:18 +0200

    fix: update logo image source in setup page

--- apps/app/src/pages/setup/setup-page.tsx
+++ apps/app/src/pages/setup/setup-page.tsx
@@ -17,7 +17,7 @@ export default function SetupPage() {
 			<div className="flex w-full max-w-sm flex-col gap-6">
 				<div className="flex items-center gap-2 self-center font-medium">
 					<div className="text-primary-foreground flex size-6 items-center justify-center rounded-md">
-						<img src="/img/logo.png" alt="logo" />
+						<img src="/img/logo-ui.png" alt="logo" />
 					</div>
 					Uptime Kotlin
 				</div>
